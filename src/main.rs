8fdb60aa1e46bad2f293907a2a023b4e50505a6c
xiuxiu62 <jacremer@live.com>
2023-03-17 09:33:10 -0700

    add shutdown hook and handle escape key event

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,21 @@ pub mod text_buffer;
 pub mod util;
 
 use crossterm::{
-    event, execute,
+    event::{self, Event, KeyCode},
+    execute,
     style::{self, Color},
     terminal,
 };
 use std::{
-    cell::RefCell,
+    cell::{RefCell, RefMut},
     collections::BTreeMap,
     io::{self, Stdout},
     path::PathBuf,
     rc::{Rc, Weak},
+    time::Duration,
 };
 use text_buffer::TextBuffer;
+use thiserror::Error;
 use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
@@ -32,40 +35,33 @@ macro_rules! btree_map {
     }}
 }
 
-fn main() -> text_buffer::Result<()> {
-    // let mut buffer = TextBuffer::load(PathBuf::from("./test-data.txt"))?;
+fn main() -> Result<()> {
     let mut editor = Editor::new()?;
     editor.initialize()?;
 
     Ok(())
-
-    // buffer.insert_line(3, "meow");
-    // Editor::default()
-
-    // buffer.save()
 }
 
-type WeakTermRef = Weak<RefCell<Terminal<CrosstermBackend<Stdout>>>>;
+type CrossTerminal = Terminal<CrosstermBackend<Stdout>>;
 
 struct Editor {
     active_buffer_id: usize,
     buffers: BTreeMap<usize, TextBuffer>,
-    terminal: WeakTermRef,
+    terminal: Option<CrossTerminal>,
 }
 
 impl Editor {
     pub fn new() -> text_buffer::Result<Self> {
-        let mut buffer = TextBuffer::load(PathBuf::from("./test-data.txt"))?;
-
         Ok(Self {
             active_buffer_id: 1,
-            buffers: btree_map![1 => buffer],
-            terminal: Weak::new(),
+            buffers: btree_map![1 => TextBuffer::load(PathBuf::from("./test-data.txt"))?],
+            terminal: None,
         })
     }
 
-    pub fn initialize(&mut self) -> io::Result<()> {
+    pub fn initialize(&mut self) -> Result<()> {
         let mut stdout = io::stdout();
+        terminal::enable_raw_mode()?;
         execute!(
             stdout,
             terminal::EnterAlternateScreen,
@@ -73,14 +69,11 @@ impl Editor {
             event::EnableMouseCapture,
             style::SetBackgroundColor(Color::Black),
             style::SetForegroundColor(Color::White),
-            style::SetForegroundColor(Color::Magenta),
+            style::SetUnderlineColor(Color::Magenta),
         )?;
-        terminal::enable_raw_mode()?;
 
         let mut terminal = Terminal::new(CrosstermBackend::new(stdout))?;
         terminal.clear()?;
-
-        // self.terminal = Weak::from(RefCell::new(terminal));
 
         terminal.draw(|frame| {
             let chunks = Layout::default()
@@ -100,7 +93,28 @@ impl Editor {
             frame.render_widget(paragraph, chunks[0]);
         })?;
 
+        let timeout = Duration::from_millis(100);
+        loop {
+            if event::poll(timeout)? {
+                if let Event::Key(key) = event::read()? {
+                    if let KeyCode::Esc = key.code {
+                        break;
+                    }
+                }
+            }
+        }
+
+        self.terminal = Some(terminal);
+
         Ok(())
+    }
+
+    fn terminal(&self) -> Result<&CrossTerminal> {
+        self.terminal.as_ref().ok_or(Error::Uninitialized)
+    }
+
+    fn terminal_mut(&mut self) -> Result<&mut CrossTerminal> {
+        self.terminal.as_mut().ok_or(Error::Uninitialized)
     }
 
     fn get_active(&self) -> Option<&TextBuffer> {
@@ -113,7 +127,45 @@ impl Default for Editor {
         Self {
             active_buffer_id: 1,
             buffers: btree_map![1 => TextBuffer::default()],
-            terminal: Weak::new(),
+            terminal: None,
         }
     }
+}
+
+impl Drop for Editor {
+    fn drop(&mut self) {
+        let backend = match self.terminal_mut() {
+            Ok(terminal) => terminal.backend_mut(),
+            // Terminal was never initialized, so we don't need to run the shutdown hook
+            Err(Error::Uninitialized) => return,
+            _ => unreachable!(),
+        };
+
+        let handle_error = |result| {
+            if let Err(err) = result {
+                eprintln!("{err}");
+            }
+        };
+
+        handle_error(execute!(
+            backend,
+            terminal::LeaveAlternateScreen,
+            terminal::DisableLineWrap,
+            event::DisableMouseCapture
+        ));
+
+        handle_error(terminal::disable_raw_mode());
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error(transparent)]
+    TextBuffer(#[from] text_buffer::Error),
+    #[error("Editor unintialized")]
+    Uninitialized,
 }
